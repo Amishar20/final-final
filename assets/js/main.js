cb25f9ec216e46e5f3d1c165d2042cd7dd14b300
Ridhi Aggarwal <127647709+Nox1916@users.noreply.github.com>
2023-12-21 12:33:30 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -89,7 +89,7 @@ document.addEventListener('DOMContentLoaded', () => {
     speed: 600,
     loop: true,
     autoplay: {
-      delay: 5000,
+      delay: 300000,
       disableOnInteraction: false
     },
     slidesPerView: 'auto',
